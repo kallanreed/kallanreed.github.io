01e8b088feae43988ac0723d2a4236f4bae5dda8
kallanreed <kallanreed@outlook.com>
2023-03-05 20:18:36 -0800

    Faster and prettier fields.js

--- processing/fields/fields.js
+++ processing/fields/fields.js
@@ -4,24 +4,28 @@ class FieldEmitter {
     this.y = y;
     this.dx = random(2) - 1;
     this.dy = random(2) - 1;
-    this.strength = random(10, 30);
+    this.size = windowWidth / 1000 * random(20, 40);
   }
-  
+
   getForce(x, y) {
     var d = dist(x, y, this.x, this.y);
-    var dx = (x - this.x) / d;
-    var dy = (y - this.y) / d;
-    var mul = (this.strength / d);
+
+    if (d === 0) {
+      return { dx: 0, dy: 0 };
+    }
+
+    var dx = this.size * (x - this.x) / (d * d);
+    var dy = this.size * (y - this.y) / (d * d);
     
-    return { dx: dx * mul, dy: dy * mul };
+    return { dx: dx, dy: dy };
   }
   
   update() {
     this.x += this.dx;
     this.y += this.dy;
     
-    if (this.x < 0 || this.x > displayWidth) { this.dx *= -1; }
-    if (this.y < 0 || this.y > displayHeight) { this.dy *= -1; }
+    if (this.x < 0 || this.x > windowWidth) { this.dx *= -1; }
+    if (this.y < 0 || this.y > windowHeight) { this.dy *= -1; }
   }
 }
 
@@ -29,18 +33,17 @@ class Particle {
   constructor(x) {
     this.initX = x;
     this.x = x;
-    this.y = random(displayHeight);
+    this.y = random(windowHeight);
     this.px = x;
     this.py = 0;
     this.dx = 0;
     this.dy = 0;
-    this.drag = random(5, 9) / 10;
-    this.done = false;
+    this.drag = random(6, 8) / 10;
   }
   
   update() {
     // Restoring force.
-    this.dx += (this.initX - this.x) / 10;
+    this.dx += (this.initX - this.x) / 100;
     
     this.px = this.x;
     this.py = this.y;
@@ -49,16 +52,13 @@ class Particle {
     this.dx *= this.drag;
     this.dy *= this.drag;
     
-    if (this.py > displayHeight) {
+    if (this.py > windowHeight) {
       this.y = 0;
-      this.py = 0;
+      this.py = this.y - this.py;
     }
   }
   
   draw() {
-    if (this.done) {
-      return;
-    }
     line(this.x, this.y, this.px, this.py);
   }
 }
@@ -68,49 +68,34 @@ class Board {
     this.particles = [];
     this.emitters = [];
     
-    for (var i = 0; i < displayWidth; i += 0.1) {
+    for (var i = 0; i < windowWidth; i += 0.1) {
       this.particles.push(new Particle(i));
     }
-    
-    var emitterCount = displayWidth * displayHeight / 100000;
+
+    var emitterCount = 10; //windowWidth * windowHeight / 100000;
     for (i = 0; i < emitterCount; i++) {
-      var x = random(0, displayWidth);
-      var y = random(0, displayHeight);
+      var x = random(0, windowWidth);
+      var y = random(0, windowHeight);
       this.emitters.push(new FieldEmitter(x, y));
     }
   }
   
-  update() {
-    var allDone = true;
-    
-    for (var p of this.particles) {
-      if (p.done) {
-        continue;
-      }
-      
-      allDone = false;
-      
+  draw() {
+    for (var e of this.emitters) {
+      e.update();
+    }
+
+    for (const p of this.particles) {
       // Gravitational force.
-      p.dy += 0.8;
-      
+      p.dy += 0.85;
+
       for (var e of this.emitters) {
         var f = e.getForce(p.x, p.y);
         p.dx += f.dx;
         p.dy += f.dy;
       }
-      
+
       p.update();
-    }
-    
-    for (var e of this.emitters) {
-      e.update();
-    }
-    
-    return allDone;
-  }
-  
-  draw() {
-    for (const p of this.particles) {
       p.draw();
     }
   }
@@ -119,7 +104,7 @@ class Board {
 var board = null;
 
 function setup() {
-  createCanvas(displayWidth, displayHeight);
+  createCanvas(windowWidth, windowHeight);
   background(0);
   board = new Board();
 }
@@ -130,10 +115,14 @@ function windowResized() {
 }
 
 function draw() {
-  if (board.update()) {
-    noLoop();
-  }
   background(0, 8);
   stroke(255, 50);
   board.draw();
+}
+
+function mousePressed()
+{
+  var e = random(board.emitters);
+  e.x = mouseX;
+  e.y = mouseY;
 }
